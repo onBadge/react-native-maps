394765520ce92f6db3c373ecd58b0f6d9d68cb86
Max Vasin <maksim.vasin@devexpress.com>
2017-10-18 19:44:16 +0300

    - ios: optimized marker shading

--- lib/ios/AirGoogleMaps/AIRGoogleMapBadgeMarker.m
+++ lib/ios/AirGoogleMaps/AIRGoogleMapBadgeMarker.m
@@ -35,7 +35,7 @@ CIContext* g_ciContext;
   if (cancel) {
     cancel();
   }
-
+  
   if (!source) {
     complete(nil, nil);
     return nil;
@@ -43,71 +43,54 @@ CIContext* g_ciContext;
   
   return [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:source]
                                                  size:_size
-                                                   scale:1
-                                                 clipped:YES
-                                              resizeMode:RCTResizeModeCenter
-                                           progressBlock:nil
-                                        partialLoadBlock:nil
-                                         completionBlock:complete];
+                                                scale:1
+                                              clipped:YES
+                                           resizeMode:RCTResizeModeCenter
+                                        progressBlock:nil
+                                     partialLoadBlock:nil
+                                      completionBlock:complete];
   
-}
-
-- (UIImage*)blankImage:(CGSize)imageSize scale:(CGFloat)scale {
-  UIGraphicsBeginImageContextWithOptions(imageSize, NO, scale);
-  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  return image;
-}
-
-- (UIImage*)overlayImage:(CGSize)imageSize fill:(UIColor*)fill scale:(CGFloat)scale {
-  UIGraphicsBeginImageContextWithOptions(imageSize, YES, scale);
-  [fill setFill];
-  UIRectFill(CGRectMake(0, 0, imageSize.width, imageSize.height));
-  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  return image;
 }
 
 - (UIImage*)maskImage:(UIImage*)source mask:(UIImage*)mask overlay:(UIImage*)overlay {
   CIImage* ciSource = [CIImage imageWithCGImage:source.CGImage];
   CIImage* ciMask = [CIImage imageWithCGImage:mask.CGImage];
-//  CIImage* ciBackground = [CIImage imageWithCGImage:[self blankImage:_size scale:source.scale].CGImage];
- // CIImage* ciOverlayBackground = [CIImage imageWithCGImage:[self overlayImage:_size fill:_pinColor scale:source.scale].CGImage];
   CIImage* ciBackground = [[CIImage imageWithColor:[CIColor clearColor]] imageByCroppingToRect:CGRectMake(0, 0, source.size.width, source.size.height)];
   CIImage* ciOverlayBackground = [[CIImage imageWithColor:[CIColor colorWithCGColor:_pinColor.CGColor]] imageByCroppingToRect:CGRectMake(0, 0, source.size.width, source.size.height)];
   CIImage* ciOverlay = [CIImage imageWithCGImage:overlay.CGImage];
   
   CIContext* context = [AIRGoogleMapBadgeMarker ciContext];
   
-  CIFilter* maskFilter = [CIFilter filterWithName:@"CIBlendWithAlphaMask"];
-  [maskFilter setValue:ciSource forKey:@"inputImage"];
-  [maskFilter setValue:ciBackground forKey:@"inputBackgroundImage"];
-  [maskFilter setValue:ciMask forKey:@"inputMaskImage"];
-
-  CIImage* maskResult = maskFilter.outputImage;
+  CIFilter* badgeFilter = [CIFilter filterWithName:@"CIBlendWithAlphaMask"
+                               withInputParameters:@{
+                                                     @"inputImage": ciSource,
+                                                     @"inputBackgroundImage": ciBackground,
+                                                     @"inputMaskImage": ciMask
+                                                     }];
+  CIImage* badgeImage = [badgeFilter.outputImage imageByApplyingFilter:@"CIColorControls"
+                                                   withInputParameters:@{
+                                                                         @"inputSaturation": @(1.0),
+                                                                         @"inputBrightness": @(0.4),
+                                                                         @"inputContrast": @(1.0)
+                                                                         }];
   
-  CIFilter* gammaCoorection = [CIFilter filterWithName:@"CIColorControls"];
-  [gammaCoorection setValue:maskResult forKey:@"inputImage"];
-  [gammaCoorection setValue:@(1.0) forKey:@"inputSaturation"];
-  [gammaCoorection setValue:@(0.4) forKey:@"inputBrightness"];
-  [gammaCoorection setValue:@(1.0) forKey:@"inputContrast"];
   
-  maskResult = gammaCoorection.outputImage;
-  
-  CIFilter* overlayMask = [CIFilter filterWithName:@"CIBlendWithAlphaMask"];
-  [overlayMask setValue:ciOverlayBackground forKey:@"inputImage"];
-  [overlayMask setValue:ciBackground forKey:@"inputBackgroundImage"];
-  [overlayMask setValue:ciOverlay forKey:@"inputMaskImage"];
-
-  CIImage* overlayResult = overlayMask.outputImage;
+  CIFilter* overlayFilter = [CIFilter filterWithName:@"CIBlendWithAlphaMask"
+                                 withInputParameters:@{
+                                                       @"inputImage": ciOverlayBackground,
+                                                       @"inputBackgroundImage": ciBackground,
+                                                       @"inputMaskImage": ciOverlay
+                                                       }];
   
   CIFilter* finalFilter = [CIFilter filterWithName:@"CISourceOverCompositing"];
-  [finalFilter setValue:overlayResult forKey:@"inputImage"];
-  [finalFilter setValue:maskResult forKey:@"inputBackgroundImage"];
+  [finalFilter setValue:overlayFilter.outputImage forKey:@"inputImage"];
+  [finalFilter setValue:badgeImage forKey:@"inputBackgroundImage"];
   
   CIImage* final = finalFilter.outputImage;
   
-  return [UIImage imageWithCGImage:[context createCGImage:final fromRect:final.extent] scale:source.scale * 2 orientation:source.imageOrientation];
+  return [UIImage imageWithCGImage:[context createCGImage:final fromRect:final.extent]
+                             scale:RCTScreenScale()
+                       orientation:source.imageOrientation];
 }
 
 - (void)makeIcon {
@@ -120,7 +103,7 @@ CIContext* g_ciContext;
     _realMarker.icon = icon;
     if (_realMarker.icon)
       _realMarker.opacity = 1;
-      _realMarker.groundAnchor = CGPointMake(0.5f, 0.75f);
+    _realMarker.groundAnchor = CGPointMake(0.5f, 0.75f);
   });
 }
 
